b83c318628aaee672a4f58f3539c63ded46a110b
Corentin <corentin.kistler@edu.devinci.fr>
2023-07-04 15:08:36 +0200

    commit

--- test/getRandomNumberTest.js
+++ test/getRandomNumberTest.js
@@ -4,6 +4,6 @@ describe('getRandomNumber', function() {
     it('should return a random number between 0 and 100', function() {
       const randomNumber = getRandomNumber();
       assert.strictEqual(typeof randomNumber, 'number');
-      assert.ok(randomNumber >= 0 && randomNumber <= 100);
+      assert.ok(randomNumber >= 0 && randomNumber <= 10000000);
     });
   });
